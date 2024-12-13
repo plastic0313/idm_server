58e1052be4166f39a1f50b27d6fc4f5be71fb96c
Pengfei Yu <731089648@qq.com>
2024-12-13 15:25:44 +0800

    Update get_viton.py

--- InstantID-faceswap/server/get_viton.py
+++ InstantID-faceswap/server/get_viton.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Time    : 2021/09/02 19:06
-# @Author  : lishanlu
-# @File    : post_request_samples.py
-# @Software: PyCharm
+# @Time    : 2024/12/13 19:06
+# @Author  : yupengfei
+# @File    : get_viton.py
 # @Discription: 上传图片及参数的不同方式
 
 from __future__ import absolute_import, print_function, division
@@ -14,7 +13,6 @@ import json
 from PIL import Image
 import io
 import time
-
 
 
 person_image_path = '../examples/hf_test/test_input.png'
@@ -41,21 +39,21 @@ def post_json():
     model_image = cv2.imread(model_image_path)
     model_image_str = img2b64(model_image)
 
-    data = {'person_images': [person_image_str], 'model_image': model_image_str, 'name': 'xyz', 'age':33}
-    response = requests.post('http://43.128.133.3:80/predict', json=data).json()
+    data = {'person_images': [person_image_str], 'model_image': model_image_str, 'model_pic_id': 0, 'person_id': 0}
+    # response = requests.post('http://43.128.133.3:80/predict', json=data).json()
+    response = requests.post('http://127.0.0.1:80/predict', json=data).json()
 
-    res_image = base64_to_pil_image(response['model_image_res'])
-
-    res_image.save('res_test.png')
-
-
+    if response['status']:
+        print(response['info']) 
+        print(f"推理耗时: {response['cost_time']} 秒") 
+        res_image = base64_to_pil_image(response['model_image_res'])
+        res_image.save('res_test.png')
+    else:
+        print(response['info']) 
 
 if __name__ == '__main__':
     start_time = time.time()
     post_json()
     end_time = time.time()
     elapsed_time = end_time - start_time
-    print(f"耗时: {elapsed_time} 秒")
-
-
-
+    print(f"调用耗时: {elapsed_time} 秒")
